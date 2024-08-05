9c6bd650beefead903779e65dc2de9a1518081cc
droxy <chiomaotu22@gmail.com>
2024-08-05 14:09:54 +0100

    added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-red-400 text-3xl bg-green-400">Hello</h1>
-
-      <p className="text-blue-300 bg-red-500">work work</p>
     </div>
   );
 }
