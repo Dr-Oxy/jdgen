dd0e0b8425c469d1f902a65e4a2e0d6086000b12
droxy <chiomaotu22@gmail.com>
2024-08-05 14:03:29 +0100

    add react and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-red-400 text-3xl bg-green-400">Hello</h1>
+
+      <p className="text-blue-300 bg-red-500">work work</p>
+    </div>
+  );
+}
+
+export default App;
