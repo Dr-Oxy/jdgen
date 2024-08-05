1e4db5d8cc0207ebe96a195e8ceb7f5ddd628e8b
droxy <chiomaotu22@gmail.com>
2024-08-05 17:09:20 +0100

    created select component

---
+++ src/components/elements/Select.tsx
@@ -0,0 +1,116 @@
+// import { DropdownIcon } from '@/utilities/DropdownIcon';
+import React, { ReactNode } from 'react';
+import Select from 'react-select';
+
+export interface DropdownTypes {
+  label: string;
+  value: string | number | ReactNode;
+}
+
+interface SelectDropdownTypes {
+  defaultValue?: any;
+  onChange?: any;
+  styles?: string | any;
+  disabled?: boolean;
+  options: { value: string | number; label: string }[];
+  textTransform?: string;
+  error?: string | undefined;
+  icon?: number;
+  leftIcon?: ReactNode;
+  placeholder?: string;
+  required?: boolean;
+}
+
+const SelectDropdown = React.forwardRef(function SelectDropdown(
+  {
+    defaultValue,
+    onChange,
+    styles,
+    disabled,
+    options,
+    textTransform,
+    error,
+    icon,
+    leftIcon,
+    placeholder,
+    required,
+    ...rest
+  }: SelectDropdownTypes,
+  ref,
+) {
+  // select dropdown custom styles
+  const selectCustomStyles = {
+    menu: (provided: any) => ({
+      ...provided,
+      fontSize: '13px',
+      textTransform: textTransform || 'capitalize',
+      zIndex: 100,
+    }),
+
+    placeholder: (provided: any) => ({
+      ...provided,
+      color: '#bdbdbd',
+      fontSize: '13px',
+    }),
+
+    control: (provided: any, state: { isFocused: any }) => ({
+      ...provided,
+      paddingLeft: leftIcon ? '32px' : '8',
+      minHeight: '49px',
+      fontSize: '16px !important',
+      border: `1px solid ${state.isFocused ? 'black' : 'black'}`,
+      color: '#E7EDF2',
+      borderRadius: '8px',
+      textTransform: textTransform || 'capitalize',
+      backgroundColor: 'transparent',
+    }),
+
+    option: (provided: any, state: { isSelected: any; isFocused: any }) => ({
+      ...provided,
+      zIndex: 100,
+      fontSize: '13px',
+      backgroundColor: state.isSelected
+        ? '#3A3A3A'
+        : state.isFocused
+        ? '#e6e9f0'
+        : '',
+    }),
+
+    singleValue: (provided: any, state: { isDisabled: any }) => {
+      const opacity = state.isDisabled ? 0.5 : 1;
+      const transition = 'opacity 300ms';
+
+      return { ...provided, opacity, transition };
+    },
+  };
+
+  return (
+    <div className="w-full">
+      <div className="relative flex w-full items-center gap-2">
+        {leftIcon && (
+          <div className="text-dark absolute left-3 top-1/2 z-50 -translate-y-[50%] text-lg">
+            {leftIcon}
+          </div>
+        )}
+        <div className="relative flex-1">
+          <Select
+            components={{
+              IndicatorSeparator: () => null,
+            }}
+            placeholder={placeholder || options?.[0]?.label}
+            isDisabled={disabled}
+            defaultValue={defaultValue}
+            onChange={onChange}
+            styles={styles || selectCustomStyles}
+            options={options}
+            {...rest}
+          />
+        </div>
+      </div>
+
+      {error && <div className="ml-1 mt-2 text-xsm text-red-500">{error}</div>}
+    </div>
+  );
+});
+
+export default SelectDropdown;
